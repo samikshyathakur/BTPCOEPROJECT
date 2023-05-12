54015d3b68bdd8136c410c4b437aec99344f87d5
samikshyathakur <samikshyathakur09@gmail.com>
2023-05-12 10:09:55 +0000

    GIT v1 message

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["btpcoeproject/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"btpcoeproject/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
